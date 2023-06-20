158c7e726aa45960e8021018cce6f2e72bed2993
Nicolas Fuentes <nfuentes11@gmail.com>
2023-06-19 20:42:32 -0400

    30days day 2

---
+++ script.js
@@ -0,0 +1,17 @@
+//Day 2 first time around
+// let word = "JavaScript";
+// word[0] = "y";
+// console.log(word);
+
+// let nums = [4, "Hello, fat fuck", 11, 3];
+// let nums2 = nums;
+// nums2[1] = "sos un cornudo";
+// console.log(nums);
+// console.log(nums2);
+
+const pie = Math.PI;
+const PI = Math.ceil(pie);
+console.log(PI);
+
+const random = Math.floor(Math.random() * 101);
+console.log(random);
