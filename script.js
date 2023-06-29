4d132b04a2dceaea60ec1bc01aad5db8a9cf654e
Nicolas Fuentes <nfuentes11@gmail.com>
2023-06-28 23:54:07 -0400

    js day two

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
-// Day 3
-//firstName, lastName, country, city, age, isMarried, year
-let firstName = "Nicolas";
-let lastName = "Fuentes";
-let country = "USA";
-let city = "Kearny";
-let age = 34;
-let isMarried = false;
-let year = 2023;
-console.log(
-  typeof firstName,
-  typeof lastName,
-  typeof country,
-  typeof city,
-  typeof age,
-  typeof isMarried,
-  typeof year
-);
-console.log(typeof "10" == typeof 10);
-console.log(typeof "10" === typeof 10);
-console.log(parseFloat("9.8") === 10);
-let name = "";
-let state = null;
-let bro = undefined;
-let jojo = false;
-let popo = 0;
-let jiji = "You know what I mean";
-let gigi = 10;
-let pepe = true;
-console.log(4 > 3, "ture");
-console.log(4 >= 3, " true");
-console.log(4 < 3, "false");
-console.log(4 <= 3, " false");
-console.log(4 == 4, " ture");
-console.log(4 === 4, "true");
-console.log(4 != 4, "false");
-console.log(4 !== 4, "false");
-console.log(4 != "4", "ture");
-console.log(4 == "4", "true");
-console.log(4 === "4", "false");
-console.log(4 > 3 && 10 < 12, "true");
-console.log(4 > 3 && 10 > 12, "false");
-console.log(4 > 3 || 10 < 12, "true");
-console.log(4 > 3 || 10 > 12, "true");
-console.log(!(4 > 3), " false");
-console.log(!(4 < 3), " ture");
-console.log(!false, " true");
-console.log(!(4 > 3 && 10 < 12), "false");
-console.log(!(4 > 3 && 10 > 12), "ture");
-console.log(!(4 === "4"), " ture");
-// There is no 'on' in both dragon and python
-console.log(!("dragon", `python`).includes("on"));
+const time = new Date();
+console.log(time);
+const month = time.getMonth() + 1;
+const day = time.getDate();
+console.log(day);
+const dayNum = time.getDay();
+console.log(dayNum);
+const hh = time.getHours();
+const mm = time.getMinutes();
+const sec = time.getSeconds();
+const timeSinceTheSenventies = time.getTime();
+const base = prompt("Enter the base of a Triangel", "Enter number here");
+const hight = prompt("Enter hight of the triangel", "Enter number here");
+const areaOfaTriangel = alert(0.5 * base * hight);
