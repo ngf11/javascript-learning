d56b1d9579f101fb806807c94a8445e5ad8c4f2c
Nicolas Fuentes <nfuentes11@gmail.com>
2023-06-29 23:20:44 -0400

    30DayJS day 3

--- script.js
+++ script.js
@@ -9,6 +9,32 @@ const hh = time.getHours();
 const mm = time.getMinutes();
 const sec = time.getSeconds();
 const timeSinceTheSenventies = time.getTime();
-const base = prompt("Enter the base of a Triangel", "Enter number here");
-const hight = prompt("Enter hight of the triangel", "Enter number here");
-const areaOfaTriangel = alert(0.5 * base * hight);
+// const base = prompt("Enter the base of a Triangel", "Enter number here");
+// const hight = prompt("Enter hight of the triangel", "Enter number here");
+// const areaOfaTriangel = alert(0.5 * base * hight);
+// const sideA = prompt("Enter side A here");
+// const sideB = prompt("Enter side B here");
+// const sideC = prompt("Enter side C here");
+// const perimeter = alert(
+//   `Enter side A: ${sideA}\n Enter side B:${sideB}\n Enter side C:${sideC}\n The perimeter of the triangle is ${
+//     parseFloat(sideA) + parseFloat(sideB) + parseFloat(sideC)
+//   } `
+// );
+// const length = prompt("Enter length");
+// const width = prompt("Enter width");
+// const perimeterAndArea = alert(
+//   `The area of the rectangle is ${
+//     parseFloat(length) * parseFloat(width)
+//   } and the perimeter of the rectangle is ${
+//     2 * (parseFloat(length) + parseFloat(width))
+//   }`
+// );
+// console.log(Math.ceil(Math.PI));
+// const pi = 3.14;
+// const radius = prompt("enter the radius of a circl");
+// const areaAndCincunference = alert(
+//   `The area of the circle is ${pi * parseFloat(radius) * parseFloat(radius)}`
+// );
+// Slope is m = (y2-y1) / (x2-x1). Find the slope between point (2, 2) and point(6,10)
+const sideX = prompt("Enter Side X");
+const sideY = prompt("Enter side Y");
