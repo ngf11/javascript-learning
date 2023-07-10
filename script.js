ac22d4d06bf3a7b17ddcf45f865b765e2ea73861
Nicolas Fuentes <nfuentes11@gmail.com>
2023-07-09 21:37:02 -0400

    JS day 3

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-
+let firstName = "Nicolas";
+let lastName = "Fuentes";
+let country = "United States";
+let city = "Kearny";
+let age = 34;
+let year = 1989;
+console.log(
+  typeof firstName,
+  typeof lastName,
+  typeof country,
+  typeof age,
+  typeof year,
+  typeof city
+);
+const myInfo = {
+  firstName: "Nico",
+  lastNmae: "Fuentes",
+  country: "United States",
+  city: "Kearny",
+  year: 1989,
+  age: 34,
+};
+console.log(myInfo);
+console.log(typeof "10" === typeof 10);
+console.log(parseFloat("9.8") === 10);
+let str = "I am a choad";
+let me = 11;
+let bro = true;
+let you = NaN;
+let jiji = "";
+let her = false;
+let uAre = null;
+let brb = undefined;
+console.log(4 > 3 && 10 < 12, "true");
+console.log(4 > 3 && 10 > 12, "false");
+console.log(4 > 3 || 10 < 12, "true");
+console.log(4 > 3 || 10 > 12, "true");
+console.log(!(4 > 3), "false");
+console.log(!(4 < 3), "true");
+console.log(!false, "true");
+console.log(!(4 > 3 && 10 < 12), "false");
+console.log(!(4 > 3 && 10 > 12), "true");
+console.log(!(4 === "4"), "true");
+console.log(!("dragon".includes("on"), "python".includes("on")));
+console.log(4 > 3, "ture");
+console.log(4 >= 3, "true");
+console.log(4 < 3, "false");
+console.log(4 <= 3, "false");
+console.log(4 == 4, "ture");
+console.log(4 === 4, "true");
+console.log(4 != 4, "false");
+console.log(4 !== 4, "false");
+console.log(4 != "4", "true");
+console.log(4 == "4", "false");
+console.log(4 === "4", "false");
