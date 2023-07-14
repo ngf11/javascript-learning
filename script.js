ff5600f806bd9388a1985199824cd05a120f2dae
Nicolas Fuentes <nfuentes11@gmail.com>
2023-07-14 00:39:31 -0400

    JS day 2
    '
    '

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-const month = prompt("Enter the number of the month");
-const year = prompt("Enter the year");
-const numberOfDays = getNumberOfDays(month, year);
-function getNumberOfDays(month, year) {
-  const daysInMonth = {
-    1: 31, // January
-    2: isLeapYear(year) ? 29 : 28, // February (depends on leap year)
-    3: 31, // March
-    4: 30, // April
-    5: 31, // May
-    6: 30, // June
-    7: 31, // July
-    8: 31, // August
-    9: 30, // September
-    10: 31, // October
-    11: 30, // November
-    12: 31, // December
-  };
-
-  return daysInMonth[month];
-}
-function isLeapYear(year) {
-  // Leap years are divisible by 4, but not by 100, unless they are divisible by 400
-  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-}
-getNumberOfDays();
-alert(`Number of days in month ${month} of year ${year}: ${numberOfDays}`);
+//day 2
+let challenge = "30 Days Of JavaScript";
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toLocaleUpperCase());
+console.log(challenge.toLocaleLowerCase());
+console.log(challenge.substr(0, 2));
+console.log(challenge.substring(3, 6));
+console.log(challenge.includes("Script"));
+console.log(challenge.split(""));
+console.log(challenge.split(" "));
+console.log(challenge.split(","));
+console.log(challenge.split(" , "));
+console.log(challenge.split(" ,"));
+console.log(
+  "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(", ")
+);
+console.log(
+  "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(" ,")
+);
+// console.log(challenge.replace("JavaScript", "Python"));
+console.log(challenge.charAt(15));
+console.log(challenge.charCodeAt("J"));
+console.log(
+  "You cannot end a sentence with because because because is a conjunction".indexOf(
+    "because"
+  )
+);
+console.log(
+  "You cannot end a sentence with because because because is a conjunction".lastIndexOf(
+    "because"
+  )
+);
+console.log(
+  "You cannot end a sentence with because because because is a conjunction".search(
+    "because"
+  )
+);
+console.log(" 30 Days Of JavaScript ".trim());
+console.log(challenge.startsWith("30"));
+console.log(challenge.endsWith("Script"));
+console.log(challenge.match(/a/gi));
+console.log("30 Days of".concat("JavaScript"));
+console.log(challenge.repeat(10));
