876b76c04ebe5f65daba938635cd26b2025eb4a3
Nicolas Fuentes <nfuentes11@gmail.com>
2023-07-15 23:20:45 -0400

    JS day 3

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+let hh = date.getHours();
+let mm = date.getMinutes();
+let secondsSince = date.getTime();
