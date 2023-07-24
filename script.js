217263cc0386034b07be37351458b2bfdbc0992d
Nicolas Fuentes <nfuentes11@gmail.com>
2023-07-23 23:24:13 -0400

    JS day 4

--- script.js
+++ script.js
@@ -1,1 +1,90 @@
-
+// Day 4
+// const age = prompt("enter you age");
+// let response =
+//   age >= 18
+//     ? alert(`Enter your age: ${age}\nYou are old enough to drive.`)
+//     : alert(`Enter your age: ${age}
+// You are not old enough to drive. You got ${18 - age} left to drive`);
+// const myAge = 34;
+// let yourAge = prompt("enter your age");
+// if (myAge > yourAge) {
+//   alert(
+//     `Enter your age: ${yourAge}\nI am ${myAge - yourAge} years older the you`
+//   );
+// } else if (yourAge > myAge) {
+//   alert(
+//     `Enter your age: ${yourAge}\nYou are ${yourAge - myAge} years older the me`
+//   );
+// } else {
+//   alert(`Enter your age: ${yourAge}\nWe are the same age `);
+// }
+// let a = prompt("enter a number");
+// let b = 3;
+// // a > b ? alert(`${a} is greater the ${b}`) : alert(`${b} is greater then ${a}`);
+// if (a > b) {
+//   alert(`${a} is greater the ${b}`);
+// } else if (b > a) {
+//   alert(`${b} is greater then ${a}`);
+// } else {
+//   alert(`${a} is the same number `);
+// }
+// let num = prompt("Enter any number");
+// if (num % 2 === 0) {
+//   alert(`${num} is an even number`);
+// } else {
+//   alert(`${num} is a an odd number`);
+// }
+// let grade = prompt("Enter your grader");
+// switch (true) {
+//   case grade >= 90:
+//     alert("A");
+//     break;
+//   case grade >= 80:
+//     alert("B");
+//     break;
+//   case grade >= 70:
+//     alert("C");
+//     break;
+//   case grade >= 60:
+//     alert("D");
+//     break;
+//   default:
+//     alert("F");
+// }
+// Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+// let month = prompt("What month is it");
+// let sesson = month.toLocaleLowerCase();
+// switch (true) {
+//   case sesson == "december" || sesson == "january" || sesson == "february":
+//     alert("Winter");
+//     break;
+//   case sesson == "march" || sesson == "april" || sesson == "may":
+//     alert("Spring");
+//     break;
+//   case sesson == "june" || sesson == "july" || sesson == "august":
+//     alert("summer");
+//     break;
+//   case sesson == "september" || sesson == "october" || sesson == "november":
+//     alert("Autumn");
+//     break;
+// }
+// let day = prompt("Enter a day");
+// let dayLower = day.toLocaleLowerCase();
+// switch (true) {
+//   case dayLower == "monday" ||
+//     dayLower == "tuesday" ||
+//     dayLower == "wendsday" ||
+//     dayLower == "tursday" ||
+//     dayLower == "friday":
+//     alert(`${day} that is a week day. No play`);
+//     break;
+//   case dayLower == "saturday" || dayLower == "sunday":
+//     alert(`${dayLower} is the weekend. Play time`);
+//     break;
+//   default:
+//     alert(`${day} that is not a day`);
+// }
