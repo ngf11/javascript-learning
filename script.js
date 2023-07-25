1c69435cc5e9e58a9dd99231ab179f5a9b1a77d4
Nicolas Fuentes <nfuentes11@gmail.com>
2023-07-24 21:51:57 -0400

    JS day 4

--- script.js
+++ script.js
@@ -1,90 +1,53 @@
-// Day 4
-// const age = prompt("enter you age");
-// let response =
-//   age >= 18
-//     ? alert(`Enter your age: ${age}\nYou are old enough to drive.`)
-//     : alert(`Enter your age: ${age}
-// You are not old enough to drive. You got ${18 - age} left to drive`);
-// const myAge = 34;
-// let yourAge = prompt("enter your age");
-// if (myAge > yourAge) {
-//   alert(
-//     `Enter your age: ${yourAge}\nI am ${myAge - yourAge} years older the you`
-//   );
-// } else if (yourAge > myAge) {
-//   alert(
-//     `Enter your age: ${yourAge}\nYou are ${yourAge - myAge} years older the me`
-//   );
-// } else {
-//   alert(`Enter your age: ${yourAge}\nWe are the same age `);
-// }
-// let a = prompt("enter a number");
-// let b = 3;
-// // a > b ? alert(`${a} is greater the ${b}`) : alert(`${b} is greater then ${a}`);
-// if (a > b) {
-//   alert(`${a} is greater the ${b}`);
-// } else if (b > a) {
-//   alert(`${b} is greater then ${a}`);
-// } else {
-//   alert(`${a} is the same number `);
-// }
-// let num = prompt("Enter any number");
-// if (num % 2 === 0) {
-//   alert(`${num} is an even number`);
-// } else {
-//   alert(`${num} is a an odd number`);
-// }
-// let grade = prompt("Enter your grader");
+// let enterMonth = prompt("Enter Month");
+// let month = enterMonth.toLocaleLowerCase();
 // switch (true) {
-//   case grade >= 90:
-//     alert("A");
+//   case month == "february":
+//     alert("28 Days");
 //     break;
-//   case grade >= 80:
-//     alert("B");
+//   case month == "january" ||
+//     month == "march" ||
+//     month == "may" ||
+//     month == "july" ||
+//     month == "august" ||
+//     month == "october" ||
+//     month == "december":
+//     alert("31 days");
 //     break;
-//   case grade >= 70:
-//     alert("C");
-//     break;
-//   case grade >= 60:
-//     alert("D");
+//   case month == "april" ||
+//     month == "june" ||
+//     month == "september" ||
+//     month == "november":
+//     alert("30 days");
 //     break;
 //   default:
-//     alert("F");
+//     alert("That's not a valid month");
 // }
-// Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
-// September, October or November, the season is Autumn.
-// December, January or February, the season is Winter.
-// March, April or May, the season is Spring
-// June, July or August, the season is Summer
-// let month = prompt("What month is it");
-// let sesson = month.toLocaleLowerCase();
-// switch (true) {
-//   case sesson == "december" || sesson == "january" || sesson == "february":
-//     alert("Winter");
-//     break;
-//   case sesson == "march" || sesson == "april" || sesson == "may":
-//     alert("Spring");
-//     break;
-//   case sesson == "june" || sesson == "july" || sesson == "august":
-//     alert("summer");
-//     break;
-//   case sesson == "september" || sesson == "october" || sesson == "november":
-//     alert("Autumn");
-//     break;
-// }
-// let day = prompt("Enter a day");
-// let dayLower = day.toLocaleLowerCase();
-// switch (true) {
-//   case dayLower == "monday" ||
-//     dayLower == "tuesday" ||
-//     dayLower == "wendsday" ||
-//     dayLower == "tursday" ||
-//     dayLower == "friday":
-//     alert(`${day} that is a week day. No play`);
-//     break;
-//   case dayLower == "saturday" || dayLower == "sunday":
-//     alert(`${dayLower} is the weekend. Play time`);
-//     break;
-//   default:
-//     alert(`${day} that is not a day`);
-// }
+let year = prompt("Enter any year");
+let month = prompt("Enter the nuber of month");
+const whatIsIt = numberOfDays(Number(year), Number(month));
+function leapYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function numberOfDays(year, month) {
+  const monthDays = {
+    1: 31,
+    2: leapYear(year) ? 29 : 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+  };
+  return monthDays[month];
+}
+numberOfDays();
+alert(`${year}, ${month}, ${whatIsIt}`);
