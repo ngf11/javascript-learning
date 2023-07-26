d6dfe7e02d104a71e054ea2f6ac8e43cffd6c0a8
Nicolas Fuentes <nfuentes11@gmail.com>
2023-07-25 23:27:08 -0400

    Js day 5

--- script.js
+++ script.js
@@ -1,53 +1,19 @@
-// let enterMonth = prompt("Enter Month");
-// let month = enterMonth.toLocaleLowerCase();
-// switch (true) {
-//   case month == "february":
-//     alert("28 Days");
-//     break;
-//   case month == "january" ||
-//     month == "march" ||
-//     month == "may" ||
-//     month == "july" ||
-//     month == "august" ||
-//     month == "october" ||
-//     month == "december":
-//     alert("31 days");
-//     break;
-//   case month == "april" ||
-//     month == "june" ||
-//     month == "september" ||
-//     month == "november":
-//     alert("30 days");
-//     break;
-//   default:
-//     alert("That's not a valid month");
-// }
-let year = prompt("Enter any year");
-let month = prompt("Enter the nuber of month");
-const whatIsIt = numberOfDays(Number(year), Number(month));
-function leapYear(year) {
-  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-function numberOfDays(year, month) {
-  const monthDays = {
-    1: 31,
-    2: leapYear(year) ? 29 : 28,
-    3: 31,
-    4: 30,
-    5: 31,
-    6: 30,
-    7: 31,
-    8: 31,
-    9: 30,
-    10: 31,
-    11: 30,
-    12: 31,
-  };
-  return monthDays[month];
-}
-numberOfDays();
-alert(`${year}, ${month}, ${whatIsIt}`);
+//Day 5
+const emptyArr = [];
+const numArr = [1, 2, 3, 4, 5, 6];
+const mixedDataTypes = ["nico", 34, "New Jeresy", 1989, true, "Web Developer"];
+const itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
+itCompanies.sort();
+console.log(itCompanies);
+itCompanies.reverse();
+console.log(itCompanies);
+itCompanies.slice(1);
+console.log(itCompanies.slice(3));
