933cd5190908dad2b5cd85d12bcf4fc87ecf1033
Nicolas Fuentes <nfuentes11@gmail.com>
2023-07-28 23:31:27 -0400

    Js day 6

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-// for (let i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
-// let i = 0;
-// while (i <= 10) {
-//   console.log(i);
-//   i++;
-// }
-// do {
-//   console.log(i);
-//   i++;
-// } while (i <= 9);
-// for (let i = 10; i >= 0; i--) {
-//   console.log(i);
-// }
-// let i = 11;
-// while (i >= 0) {
-//   console.log(i);
-//   i--;
-// }
-// do {
-//   console.log(i);
-//   i--;
-// } while (i >= 1);
-const num = 10;
-for (let i = 1; i <= num; i++) {
-  let row = "";
-  for (let j = 1; j <= i; j++) {
-    row += "#";
-  }
-  console.log(row);
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let codeChar = letters.split("");
+let random = Math.floor(Math.random() * codeChar.length);
+let letter = codeChar[random];
+console.log(codeChar);
+console.log(codeChar.length);
+// console.log(codeChar[random]);
+console.log(letter);
+for (let i = 0; i <= letter.length; i++) {
+  console.log(i);
 }
