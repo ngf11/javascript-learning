883f6533d5fa6d1345d5c416cd7fffa05c57c839
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-02 23:44:33 -0400

    JS day 6

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-for (let i = 1; i <= 10; i++) {
-  let outPut = "";
-  for (let j = 1; j < i; j++) {
-    outPut += "#";
-  }
-  console.log(outPut);
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+  sum += i;
 }
-for (let i = 0; i <= 10; i++) {
-  console.log(`${i} x ${i} =  ${i * i}`);
-}
-for (let i = 0; i <= 10; i++) {
-  console.log(`${i} ${i ** 2} ${i ** 3}`);
-}
+const dasums = [];
+console.log(sum);
+let sumEven = 0;
 for (let i = 0; i <= 100; i++) {
   if (i % 2 == 0) {
-    console.log(i);
+    sumEven += i;
+    dasums.push(sumEven);
   }
 }
+console.log(sumEven);
+let sumOdd = 0;
 for (let i = 0; i <= 100; i++) {
   if (i % 2 !== 0) {
-    console.log(i);
+    sumOdd += i;
+    dasums.push(sumOdd);
   }
 }
+console.log(sumOdd);
+console.log(dasums);
