74a5c03eaa989a82852b33ba9277641dcee336fa
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-04 00:15:41 -0400

    Js day 6

--- script.js
+++ script.js
@@ -22,26 +22,18 @@ const webTechs = [
   "MongoDB",
 ];
 
-const mernStack = ["MongoDB", "Express", "React", "Node"];
-const countriesUpper = [];
+const newCountriesArr = [].sort();
 for (const country of countries) {
-  countriesUpper.push(country.toUpperCase());
+  newCountriesArr.push(country);
 }
-console.log(countriesUpper);
-const countriesLength = [];
-for (const country of countries) {
-  countriesLength.push(country.length);
+console.log(newCountriesArr);
+let longestCountry = [];
+for (let i = 0; i < countries.length; i++) {
+  if (countries[i].length > longestCountry.length) {
+    longestCountry = countries[i];
+  }
 }
-console.log(countriesLength);
-const countriesInfo = [];
-for (const country of countries) {
-  countriesInfo.push([
-    country,
-    country.toUpperCase().substring(0, 3),
-    country.length,
-  ]);
-}
-console.log(countriesInfo);
+console.log(longestCountry);
 const countriesWithLand = [];
 for (const country of countries) {
   if (country.includes("land")) {
@@ -49,62 +41,19 @@ for (const country of countries) {
   }
 }
 console.log(countriesWithLand);
-const countriesWithIa = [];
+const countriesLengthFour = [];
 for (const country of countries) {
-  if (country.endsWith("ia")) {
-    countriesWithIa.push(country);
+  if (country.length == 4) {
+    countriesLengthFour.push(country);
   }
 }
-console.log(countriesWithIa);
-const countriesLengthFive = [];
+console.log(countriesLengthFour);
 for (const country of countries) {
-  if (country.length === 5) {
-    countriesLengthFive.push(country);
-  }
+  console.log(country.length);
 }
-console.log(countriesLengthFive);
-let longestCountry = [];
-for (let i = 1; i < countries.length; i++) {
-  if (countries[i].length > longestCountry.length) {
-    longestCountry = countries[i];
-  }
+let RevDevArr = [];
+for (let i = countries.length - 1; i >= 0; i--) {
+  let index = countries[i];
+  RevDevArr.push(index.toUpperCase());
 }
-console.log(longestCountry);
-let longestTech = [];
-for (let i = 1; i < webTechs.length; i++) {
-  if (webTechs[i].length > longestTech.length) {
-    longestTech = webTechs[i];
-  }
-}
-console.log(longestTech);
-const webTechsInfo = [];
-for (const tech of webTechs) {
-  webTechsInfo.push([tech, tech.length]);
-}
-console.log(webTechsInfo);
-const mernArr = [];
-for (const mern of mernStack) {
-  mernArr.push(mern[0]);
-}
-console.log(mernArr);
-let mernStr = mernArr.join("");
-console.log(mernStr);
-const fruit = ["banana", "orange", "mango", "lemon"];
-const revfruit = [];
-for (let i = fruit.length - 1; i >= 0; i--) {
-  let index = fruit[i];
-  revfruit.push(index);
-}
-console.log(revfruit);
-const fullStack = [
-  ["HTML", "CSS", "JS", "React"],
-  ["Node", "Express", "MongoDB"],
-];
-for (const stack of fullStack) {
-  console.log(stack);
-}
-for (let i = 0; i < fullStack.length; i++) {
-  for (let j = 0; j < fullStack[i].length; j++) {
-    console.log(fullStack[i][j]);
-  }
-}
+console.log(RevDevArr.sort());
