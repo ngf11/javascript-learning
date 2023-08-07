1544c8f367585a70388effc30223fe84c362fba2
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-07 00:35:44 -0400

    JS Day 02

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const now = new Date();
 console.log(now);
 let year = now.getFullYear();
 console.log(year);
-let month = now.getMonth();
+let month = now.getMonth() + 1;
 console.log(month);
 let day = now.getDate();
 console.log(day);
@@ -10,25 +10,21 @@ let date = now.getDay();
 console.log(date);
 let secondsTillNow = now.getTime();
 console.log(secondsTillNow);
-// let base = prompt("Enter the base of the trinagle");
-// let height = prompt("Enter the height of the trinagle");
-// let area = alert(
-//   `Enter base: ${base}\nEnter height: ${height}\nArea of the triangle: ${
-//     base * height * 0.5
-//   }`
-// );
-// let sideA = prompt("Enter side A");
-// let sideB = prompt("Enter side B");
-// let sideC = prompt("Enter side C");
-// let paramiter = alert(
-//   `Enter side A: ${sideA}\nEnter side B: ${sideB}\n Enter side C: ${sideC}\n paramiter : ${
-//     parseFloat(sideA) + parseFloat(sideB) + parseFloat(sideC)
-//   }`
-// );
-let legnth = prompt("Enter the length of the rectangle");
-let width = prompt("Enter the width of the rectangle");
-let areaAndPara = alert(
-  `Enter length: ${legnth}\nEnter width: ${width}\nArea of the rectangle: ${
-    legnth * width
-  }\nParamiter of the rectangle${2 * (parseFloat(legnth) + parseFloat(width))}`
-);
+let hh = now.getHours();
+let mm = now.getMinutes();
+if (month < 10) {
+  month = "0" + month;
+}
+if (day < 10) {
+  day = "0" + day;
+}
+if (hh > 12) {
+  hh = hh - 12;
+}
+if (hh < 10) {
+  hh = "0" + hh;
+}
+if (mm < 10) {
+  mm = "0" + mm;
+}
+console.log(`${month}/${day}/${year} ${mm}:${mm}`);
