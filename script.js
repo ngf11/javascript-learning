8c9e09b318ef1af2a23a7b137457fd8d70dd0f8a
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-06 23:56:25 -0400

    Js day 2

--- script.js
+++ script.js
@@ -10,4 +10,25 @@ let date = now.getDay();
 console.log(date);
 let secondsTillNow = now.getTime();
 console.log(secondsTillNow);
-let base = prompt();
+// let base = prompt("Enter the base of the trinagle");
+// let height = prompt("Enter the height of the trinagle");
+// let area = alert(
+//   `Enter base: ${base}\nEnter height: ${height}\nArea of the triangle: ${
+//     base * height * 0.5
+//   }`
+// );
+// let sideA = prompt("Enter side A");
+// let sideB = prompt("Enter side B");
+// let sideC = prompt("Enter side C");
+// let paramiter = alert(
+//   `Enter side A: ${sideA}\nEnter side B: ${sideB}\n Enter side C: ${sideC}\n paramiter : ${
+//     parseFloat(sideA) + parseFloat(sideB) + parseFloat(sideC)
+//   }`
+// );
+let legnth = prompt("Enter the length of the rectangle");
+let width = prompt("Enter the width of the rectangle");
+let areaAndPara = alert(
+  `Enter length: ${legnth}\nEnter width: ${width}\nArea of the rectangle: ${
+    legnth * width
+  }\nParamiter of the rectangle${2 * (parseFloat(legnth) + parseFloat(width))}`
+);
