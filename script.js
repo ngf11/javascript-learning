cb59a6ca3cbfaed39254bd861f908da51a38a9ff
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-08 23:13:53 -0400

    Js Day 4

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 // Day 4
+// let enterAge = prompt("Enter your age");
+// let canDrive =
+//   enterAge >= 18
+//     ? alert(`You are ${enterAge} years old. you can drive`)
+//     : alert(
+//         `You are ${enterAge} years old. You can not dirve. You have to wait ${
+//           18 - enterAge
+//         } `
+//       );
+// const myAge = 34;
+// let yourAge = prompt("How old are you?");
+// if (myAge < yourAge) {
+//   alert(
+//     `Enter age: ${yourAge}\nYou are older the me  by ${yourAge - myAge} years`
+//   );
+// } else if (myAge > yourAge) {
+//   alert(
+//     `Enter age: ${yourAge}\nI am older the you by ${myAge - yourAge} years`
+//   );
+// } else {
+//   alert(`Enter age: ${yourAge}\nWe are the same age`);
+// }
+// let a = 4;
+// let b = 9;
+// let daBiggest =
+//   a > b
+//     ? console.log(`${a} is greater the ${b}`)
+//     : console.log(`${b} is greater the ${a}`);
+// let num = prompt("enter a number");
+// if (num % 2 === 0) {
+//   alert(`Enter number: ${num}\n${num} is an even number`);
+// } else {
+//   alert(`Enter number: ${num}\n${num} is an odd number`);
+// }
+// let grade = prompt("Enter your grade");
+// switch (true) {
+//   case grade >= 90:
+//     alert("A");
+//     break;
+//   case grade >= 80:
+//     alert("B");
+//     break;
+//   case grade >= 70:
+//     alert("C");
+//     break;
+//   case grade >= 60:
+//     alert("D");
+//     break;
+//   default:
+//     alert("F");
+// }
