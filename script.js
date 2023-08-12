8124fa1f905e0a9bb1ce412655e37071e823ea41
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-11 23:27:53 -0400

    JS day 6

--- script.js
+++ script.js
@@ -23,3 +23,44 @@ const webTechs = [
 ];
 
 const mernStack = ["MongoDB", "Express", "React", "Node"];
+const upperCountries = [];
+for (const country of countries) {
+  console.log(country.toUpperCase());
+  upperCountries.push(country.toUpperCase());
+}
+console.log(upperCountries);
+const lengthCountries = [];
+for (const country of countries) {
+  lengthCountries.push(country.length);
+}
+console.log(lengthCountries);
+const countryInfo = [];
+for (const country of countries) {
+  countryInfo.push([
+    country,
+    country.toUpperCase().slice(0, 3),
+    country.length,
+  ]);
+}
+console.log(countryInfo);
+const landCounty = [];
+for (country of countries) {
+  if (country.includes("land")) {
+    landCounty.push(country);
+  }
+}
+console.log(landCounty);
+let longestCountries = [];
+for (let i = 0; i < countries.length; i++) {
+  if (countries[i].length > longestCountries.length) {
+    longestCountries = countries[i];
+  }
+}
+console.log(longestCountries);
+let longestTech = [];
+for (let i = 0; i < webTechs.length; i++) {
+  if (webTechs[i].length > longestTech.length) {
+    longestTech = webTechs[i];
+  }
+}
+console.log(longestTech);
