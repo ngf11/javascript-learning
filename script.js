6954b99b023c717c9699fb46885ce295bf84dad4
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-13 22:23:33 -0400

    Js day 7

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // day 7
+function sumArr(arr) {
+  let sum = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    sum += i;
+  }
+  return sum;
+}
+const nums = [1, 2, 3, 4, 5];
+console.log(sumArr(nums));
+function sumAll() {
+  let sum = 0;
+  for (let i = 0; i <= arguments.length; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(sumAll(1, 2, 3, 4, 5));
