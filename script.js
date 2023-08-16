4bac5ab3d985802bc6aa4c3a375a45ce0f010abb
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-15 23:43:55 -0400

    Js Day 7

--- script.js
+++ script.js
@@ -1,46 +1,13 @@
-function swapValues(x, y) {
-  let temp = x;
-  x = y;
-  y = temp;
-
-  return `x => ${x}, y => ${y}`;
-}
-console.log(swapValues(4, 3));
-const reverseArray = (arr) => {
-  let revArr = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-    let index = arr[i];
-    revArr.push(index);
+// 00-1B-63-84-45-E6.
+const macLetters = ["a", "b", "c", "d", "f", "A", "B", "C", "D", "F"];
+const macNums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+function randomMacAddress() {
+  let macLettersNum = macLetters.concat(macNums);
+  let macAdd = [];
+  for (let i = 0; i < 12; i++) {
+    let random = Math.floor(Math.random() * macLettersNum.length);
+    macAdd.push(macLettersNum[random]);
   }
-  return revArr;
-};
-console.log(reverseArray([1, 2, 3, 4, 5]));
-//[5, 4, 3, 2, 1]
-console.log(reverseArray(["A", "B", "C"]));
-//['C', 'B', 'A']
-function capitalizeArray(arr) {
-  let arrCap = [];
-  for (const items of arr) {
-    arrCap.push(items.toUpperCase());
-  }
-  return arrCap;
+  return macAdd.join("");
 }
-const user = ["nico", "fuentes", "didi", "wu", "tinti"];
-console.log(capitalizeArray(user));
-function sumOfNumbers(arr) {
-  let sum = 0;
-  for (let i = 0; i <= arr.length; i++) {
-    sum += i;
-  }
-  return sum;
-}
-console.log(sumOfNumbers([1, 2, 3, 4, 5]));
-function removeItem(arr) {
-  let empty = [];
-  for (let i = 0; i <= arr.length; i++) {
-    empty = arr.splice(0, arr.length - 1);
-  }
-  return empty;
-}
-console.log(removeItem([1, 2, 3, 4, 5]));
-function addItem(item) {}
+console.log(randomMacAddress());
