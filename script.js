0f9ee1c3ccf4ae0047df0ecb40f5e5cf652f89bd
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-18 23:24:51 -0400

    JS day 7

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ let rgbColorGeneratorArry = () => {
 };
 rgbColorGeneratorArry();
 console.log(rgbColorGeneratorArry());
+let rgbColorGeneratorArry = () => {
+  let num = parseInt(prompt("How many colors do you need?"));
+  let rgb = "".split(", ");
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j <= 3; j++) {
+      let random = Math.floor(Math.random() * 256);
+      rgb += random;
+    }
+  }
+  return rgb;
+};
+rgbColorGeneratorArry();
+console.log(rgbColorGeneratorArry());
