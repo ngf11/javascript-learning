4e3acb0cedd6d7181f8bf7f7c8b79c363e0301cd
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-18 23:29:46 -0400

    JS day 7

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-let rgbColorGeneratorArry = () => {
-  let num = parseInt(prompt("How many colors do you need?"));
-  let rgb = "".split(", ");
-  for (let i = 0; i < num; i++) {
-    for (let j = 0; j <= 3; j++) {
-      let random = Math.floor(Math.random() * 256);
-      rgb += random;
-    }
-  }
-  return rgb;
-};
-rgbColorGeneratorArry();
-console.log(rgbColorGeneratorArry());
-let rgbColorGeneratorArry = () => {
-  let num = parseInt(prompt("How many colors do you need?"));
-  let rgb = "".split(", ");
-  for (let i = 0; i < num; i++) {
-    for (let j = 0; j <= 3; j++) {
-      let random = Math.floor(Math.random() * 256);
-      rgb += random;
-    }
-  }
-  return rgb;
-};
-rgbColorGeneratorArry();
-console.log(rgbColorGeneratorArry());
+// let rgbColorGeneratorArry = () => {
+//   let num = parseInt(prompt("How many colors do you need?"));
+//   let rgb = "".split(", ");
+//   for (let i = 0; i < num; i++) {
+//     for (let j = 0; j <= 3; j++) {
+//       let random = Math.floor(Math.random() * 256);
+//       rgb += random;
+//     }
+//   }
+//   return rgb;
+// };
+// rgbColorGeneratorArry();
+// console.log(rgbColorGeneratorArry());
+const amountHex = prompt("How many colors do you need?");
+const 
