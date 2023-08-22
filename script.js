014ed4295aadbe31ad89b831823523ffa535e453
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-22 07:16:33 -0400

    JS day 07

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function isEmpty(item) {}
+function isEmpty(item) {
+  if (item.length == 0) {
+    return `${item} is empty`;
+  }
+}
 let nico;
 let you = "";
 let bro = null;
 let milo = undefined;
 let arr = [];
+console.log(
+  isEmpty(nico),
+  isEmpty(you),
+  isEmpty(bro),
+  isEmpty(milo),
+  isEmpty(arr)
+);
