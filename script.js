b225a68b54661d1865960640052d80ebfdb0f004
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-23 07:09:06 -0400

    JS day 7

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function isEmpty(item) {
-  if (item.length == 0 || item == null) {
-    return `${item} is empty`;
-  }
+  return item === null || item === undefined ? "is empty" : "Has information ";
 }
 let nico;
 let you = "";
@@ -9,9 +7,49 @@ let bro = null;
 let milo = undefined;
 let arr = [];
 console.log(
-  isEmpty(nico),
-  isEmpty(you),
-  isEmpty(bro),
-  isEmpty(milo),
-  isEmpty(arr)
+  [isEmpty(nico)],
+  [isEmpty(you)],
+  [isEmpty(bro)],
+  [isEmpty(milo)],
+  [isEmpty(arr)]
 );
+function sum(...arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+console.log(sum(1, 2, 3, 4, 5));
+function sumOfArrayItems(arr) {
+  let sum = 0;
+  for (const items of arr) {
+    sum += items;
+  }
+  return sum;
+}
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(sumOfArrayItems(nums));
+function average(arr) {
+  let sum = 0;
+  for (const items of arr) {
+    sum += items;
+  }
+  let sumAverage = sum / arr.length;
+  return sumAverage;
+}
+console.log(average(nums));
+const modifyArray = (arr) => {
+  let modifyArr = [];
+  for (const itmes of arr) {
+    if (itmes.length >= 5) {
+      itmes[5] = itmes[5].toUpperCase();
+      modifyArr.push(item);
+      return modifyArr;
+    } else {
+      return "Items not found";
+    }
+  }
+};
+console.log(modifyArray(["siggy", "didi"]));
+console.log(modifyArray(["siggy", "didi", "nico", "milo", "choad"]));
