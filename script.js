650bd6c5698a2b2bb55f9729a5afcbcbe9519492
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-29 07:27:59 -0400

    Js day 2

--- script.js
+++ script.js
@@ -1,48 +1,14 @@
-//day 2
-let firstname = "nico";
-let lastname = "Funtes";
-let country = "United States";
-let state = "New Jersey";
-let age = 34;
-let isMarried = false;
-console.log(
-  typeof firstname,
-  typeof lastname,
-  typeof lastname,
-  typeof country,
-  typeof state,
-  typeof age,
-  typeof isMarried
+const now = new Date();
+let day = now.getDate();
+let month = now.getMonth();
+let year = now.getFullYear();
+let hh = now.getHours();
+let mm = now.getMinutes();
+
+let hoursWorked = prompt("How many hours have you worked?");
+let ratePerHour = prompt("What your rate per hour?");
+let earnings = alert(
+  `Enter hours: ${hoursWorked}\nEnter rate: ${ratePerHour}\nYour weekly Earnings:${
+    hoursWorked * ratePerHour
+  }`
 );
-console.log(typeof "10" === typeof 10);
-console.log(parseInt("9.8") === 10);
-let me = "You are a Softwear developer";
-let nico = true;
-let yourAre = 100;
-let lie = null;
-let poop = undefined;
-let miloBehaivor = 0;
-console.log(4 > 3, true);
-console.log(4 >= 3, true);
-console.log(4 < 3, false);
-console.log(4 <= 3, false);
-console.log(4 == 4, true);
-console.log(4 === 4, true);
-console.log(4 != 4, false);
-console.log(4 !== 4, false);
-console.log(4 != "4", false);
-console.log(4 == "4", true);
-console.log(4 === "4", false);
-console.log("python".length !== "jargon".length);
-console.log(4 > 3 && 10 < 12, true);
-console.log(4 > 3 && 10 > 12, false);
-console.log(4 > 3 || 10 < 12, true);
-console.log(4 > 3 || 10 > 12, true);
-console.log(!(4 > 3), false);
-console.log(!(4 < 3), true);
-console.log(!false, true);
-console.log(!(4 > 3 && 10 < 12), false);
-console.log(!(4 > 3 && 10 > 12), true);
-console.log(!(4 === "4"), true);
-console.log(!"dargon".includes("on"));
-console.log(!"python".includes("on"));
