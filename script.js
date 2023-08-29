8f2f4f5783fc87d4498c0b47ab435a23e8617aa4
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-29 06:28:42 -0400

    Js day 2

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-
+//day 2
+let firstname = "nico";
+let lastname = "Funtes";
+let country = "United States";
+let state = "New Jersey";
+let age = 34;
+let isMarried = false;
+console.log(
+  typeof firstname,
+  typeof lastname,
+  typeof lastname,
+  typeof country,
+  typeof state,
+  typeof age,
+  typeof isMarried
+);
+console.log(typeof "10" === typeof 10);
+console.log(parseInt("9.8") === 10);
+let me = "You are a Softwear developer";
+let nico = true;
+let yourAre = 100;
+let lie = null;
+let poop = undefined;
+let miloBehaivor = 0;
+console.log(4 > 3, true);
+console.log(4 >= 3, true);
+console.log(4 < 3, false);
+console.log(4 <= 3, false);
+console.log(4 == 4, true);
+console.log(4 === 4, true);
+console.log(4 != 4, false);
+console.log(4 !== 4, false);
+console.log(4 != "4", false);
+console.log(4 == "4", true);
+console.log(4 === "4", false);
+console.log("python".length !== "jargon".length);
+console.log(4 > 3 && 10 < 12, true);
+console.log(4 > 3 && 10 > 12, false);
+console.log(4 > 3 || 10 < 12, true);
+console.log(4 > 3 || 10 > 12, true);
+console.log(!(4 > 3), false);
+console.log(!(4 < 3), true);
+console.log(!false, true);
+console.log(!(4 > 3 && 10 < 12), false);
+console.log(!(4 > 3 && 10 > 12), true);
+console.log(!(4 === "4"), true);
+console.log(!"dargon".includes("on"));
+console.log(!"python".includes("on"));
