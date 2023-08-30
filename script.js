0c64df26aeed0b48d6416fc867605b1aad004d27
Nicolas Fuentes <nfuentes11@gmail.com>
2023-08-30 09:58:01 -0400

    Js day 3

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ let month = now.getMonth();
 let year = now.getFullYear();
 let hh = now.getHours();
 let mm = now.getMinutes();
-
-let hoursWorked = prompt("How many hours have you worked?");
-let ratePerHour = prompt("What your rate per hour?");
-let earnings = alert(
-  `Enter hours: ${hoursWorked}\nEnter rate: ${ratePerHour}\nYour weekly Earnings:${
-    hoursWorked * ratePerHour
-  }`
-);
+if (day < 10) {
+  day = "0" + day;
+}
+if (month > 10) {
+  month = "0" + month;
+}
+if (hh > 12) {
+  hh = hh - 12;
+}
+if (hh < 10) {
+  hh = "0" + hh;
+}
+if (mm < 10) {
+  mm = "0" + mm;
+}
+console.log(now);
+console.log(hh);
+console.log(mm);
+console.log(`${day}.${month}.${year} ${hh}:${mm}`);
