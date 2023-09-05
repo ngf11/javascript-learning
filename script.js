761f6440aea9847d1601e11acc8380e6a44e52e7
Nicolas Fuentes <nfuentes11@gmail.com>
2023-09-04 20:50:40 -0400

    Js day 2

--- script.js
+++ script.js
@@ -49,3 +49,45 @@ console.log(typeof "10" === typeof 10);
 console.log(typeof parseInt("10") === typeof 10);
 console.log(parseFloat("9.8") === 10);
 console.log(Math.round(parseFloat("9.8")) === 10);
+console.log("python".includes("on"), "jargon".includes("on"));
+console.log("I hope this course is not full of jargon".includes("jargon"));
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.floor(Math.random() * 50) + 50);
+console.log(Math.floor(Math.random() * 256));
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log("fizz");
+//   } else if (i % 3 === 0) {
+//     console.log("buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+let str = "JavaScript";
+let splitStr = str.split("");
+let random = Math.floor(Math.random() * splitStr.length);
+console.log(splitStr[random]);
+console.log(`1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125`);
+let becauseStr =
+  "You cannot end a sentence with because because because is a conjunction";
+// becauseStr.slice(27, 31);
+console.log(becauseStr.slice(31, 54));
+let loveStr =
+  "Love is the best thing in this world. Some found their love and some are still looking for their love.";
+console.log(loveStr.match(/love/gi));
+console.log(becauseStr.match(/because/gi));
+const sentence =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+console.log(sentence.replace(/[^\w\s]/gi, ""));
+const howMuch =
+  "Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'";
+console.log(
+  howMuch
+    .match(/\d+/gi)
+    .map(Number)
+    .reduce((a, b) => {
+      return a + b;
+    })
+);
+
+// /\d+/g;
