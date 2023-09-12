bf26a43fca464caa45b6a3995ecae775598dbc89
Nicolas Fuentes <nfuentes11@gmail.com>
2023-09-12 06:41:51 -0400

    JS Day 6

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const itCompanies = [
-  "Facebook",
-  "Google",
-  "Microsoft",
-  "Apple",
-  "IBM",
-  "Oracle",
-  "Amazon",
-];
-let enterCorp = "Apple";
-let index = itCompanies.indexOf(enterCorp);
-if (index == -1) {
-  console.log("this company dose not exist");
-} else {
-  console.log(`${itCompanies[index]} does exist`);
-}
-//14 part one
+// Day 6
