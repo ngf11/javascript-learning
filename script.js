5cc7c9eace16d534ea0a04b43e427391c91c7583
Nicolas Fuentes <nfuentes11@gmail.com>
2023-09-14 06:26:58 -0400

    Js day 6

--- script.js
+++ script.js
@@ -47,3 +47,52 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+  sum += i;
+}
+console.log(sum);
+let sumEven = 0;
+let sumOdd = 0;
+let sums = [];
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    sumEven += i;
+  }
+  if (i % 2 !== 0) {
+    sumOdd += i;
+  }
+}
+sums.push(sumOdd);
+sums.push(sumEven);
+console.log(sumEven, sumOdd, sums);
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+for (let i = 0; i < 1; i++) {
+  let ranNum = [];
+  for (let j = 1; j <= 5; j++) {
+    let random = Math.floor(Math.random() * nums.length);
+    ranNum.push(nums[random]);
+  }
+  console.log(ranNum);
+}
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+shuffleArray(nums);
+let ranNum = nums.slice(0, 5);
+console.log(ranNum);
+let lettersAndNums =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxwz12345647890";
+let strSplit = lettersAndNums.split("");
+console.log(strSplit);
+for (let i = 1; i <= 1; i++) {
+  let userId = "";
+  for (let j = 1; j < 6; j++) {
+    let random = Math.floor(Math.random() * strSplit.length);
+    userId += strSplit[random];
+  }
+  console.log(userId);
+}
