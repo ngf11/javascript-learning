b56a7b1040574d0dec380fa2c0ce626553552b58
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-07 21:53:24 -0400

    Js day 7

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function rgbColorGenerator() {
   console.log(rgb);
 }
 rgbColorGenerator();
+function arrayOfHexaColors() {
+  let numberOfColors = 5;
+  const hexDeck = ["a", "b", "c", "d", "f", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  let hexArr = [];
+  for (let i = 0; i < numberOfColors; i++) {
+    let str = "";
+    for (let j = 0; j < 6; j++) {
+      let random = Math.floor(Math.random() * hexDeck.length);
+      str = hexDeck[random];
+    }
+    hexArr.push(str);
+  }
+  console.log(hexArr);
+}
+arrayOfHexaColors();
