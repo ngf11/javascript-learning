838d141b33b07d5a5e626597f767f2906377390d
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-13 13:29:33 -0400

    the struggle

--- script.js
+++ script.js
@@ -1,1 +1,100 @@
-
+/* //1
+function capFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirstLetter("nico"));
+//2
+function capfirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capfirst("nico"));
+//3
+function capfirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capfirst("nico"));
+//4
+function capfirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capfirst("nico"));
+//5
+function capFirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+console.log(capFirst("nico"));
+//6
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//7
+function capfirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capfirst("nico"));
+//8
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//9
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//10
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//11
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//12
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//13
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//14
+function capfirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capfirst("nico"));
+//15
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//16
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//17
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//18
+function capfirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+//19
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//20
+function capFirst(str) {
+  return str.charAt().toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+*/
