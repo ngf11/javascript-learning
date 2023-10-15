ee2bb47c2fe7eab9f457c17b0ed01a7aab7c015f
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-14 22:53:32 -0400

    Js day 3

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //day 3
+let firstName = "nico";
+let lastName = "Fuentes";
+let country = "United Sates";
+let state = "New Jeresey";
+let age = 34;
+let isMerried = false;
+console.log({ firstName, lastName, country, state, age, isMerried });
+console.log(typeof "10" === typeof 10);
