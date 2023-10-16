6fa5e6cbef705410586a25374f747c4225799fe0
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-16 04:56:45 -0400

    js day 3

--- script.js
+++ script.js
@@ -14,6 +14,37 @@ let isCool = true;
 let notCool = null;
 let milito = undefined;
 let bro;
+console.log(4 > 3);
+console.log(4 >= 3, true);
+console.log(4 < 3, false);
+console.log(4 <= 3, false);
+console.log(4 == 4, true);
+console.log(4 === 4, true);
+console.log(4 != 4, false);
+console.log(4 !== 4, false);
+console.log(4 != "4", false);
+console.log(4 == "4", true);
+console.log(4 === "4", false);
+console.log(!("python".length === "jargon".length));
+console.log(4 > 3 && 10 < 12, true);
+console.log(4 > 3 && 10 > 12, false);
+console.log(4 > 3 || 10 < 12, true);
+console.log(4 > 3 || 10 > 12, true);
+console.log(!(4 > 3), false);
+console.log(!(4 < 3), true);
+console.log(!false, true);
+console.log(!(4 > 3 && 10 < 12), false);
+console.log(!(4 > 3 && 10 > 12), true);
+console.log(!(4 === "4"), true);
+console.log(!("dragon".includes("on"), "python".includes("on")));
+const now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth() + 1;
+let day = now.getDate();
+let hh = now.getHours();
+let mm = now.getMinutes();
+let sinceTheSeventies = now.getTime();
+console.log({ year, month, day, hh, mm, sinceTheSeventies });
 
 // start with 30 days then strugle
 function capFirst(str) {
