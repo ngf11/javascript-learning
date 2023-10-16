5cf89d3eb3d7af462b103be010642b418e6e2d0f
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-15 21:09:14 -0400

    js day 3

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ let age = 34;
 let isMerried = false;
 console.log({ firstName, lastName, country, state, age, isMerried });
 console.log(typeof "10" === typeof 10);
+console.log(parseFloat("9.8") == 10);
+let milo = "woof";
+let didi = 3;
+let isCool = true;
+let notCool = null;
+let milito = undefined;
+let bro;
+
+// start with 30 days then strugle
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("milo"));
