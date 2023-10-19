a4cbca393c261953470f2c565e0dd88676bba576
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-19 06:01:14 -0400

    The struggle

--- script.js
+++ script.js
@@ -34,9 +34,69 @@ function capFirst(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 console.log(capFirst("gud"));
-*/
 //28
 function capFirst(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 console.log(capFirst("gud"));
+//29
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+//30
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//31
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+//32
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+//33
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+//34
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+//35
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//36
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//37
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("nico"));
+//38
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+//39
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+//40
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+*/
