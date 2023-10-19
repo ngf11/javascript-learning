afadacaafcdf539c6df9b797c7816b88e1fc7790
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-19 10:35:58 -0400

    js day 4

--- script.js
+++ script.js
@@ -1,102 +1,60 @@
-/* 
-//21
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+/* function canYouDrive() {
+  let yourAge = prompt("How old are you?");
+  return yourAge >= 18
+    ? alert(`Enter your age: ${yourAge}\nYou are old enough to drive.`)
+    : alert(
+        `Enter your age: ${yourAge}\nYou are left with ${
+          18 - yourAge
+        } years to drive.`
+      );
 }
-console.log(capFirst("milo"));
-//22
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+canYouDrive();
+function isEven() {
+    let num = prompt("Enter a number");
+    return num % 2 === 0 ? alert("Is even ") : alert("its odd");
 }
-console.log(capFirst("gud"));
-//23
-function capFrist(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+isEven();
+let yourGrade = prompt("What is your grade?", "number form");
+switch (true) {
+    case yourGrade >= 90:
+        alert("A");
+        break;
+        case yourGrade >= 80:
+            alert("B");
+            break;
+            case yourGrade >= 70:
+                alert("C");
+                break;
+                case yourGrade >= 60:
+                    alert("D");
+                    break;
+                    case yourGrade <= 50:
+                        alert("F");
+                        break;
+                        default:
+                            alert("Thats not a grade");
+                        }
+                        
+                        */
+
+function seasons() {
+  let month = prompt("What month is it?");
+  let str = month.charAt(0).toUpperCase() + month.slice(1);
+  if(str =="December" || str == "January" || str =="February"){
+      alert("Winter")
+  }else if (str =="March" || str == "April" || str =="May"){
+    alert("Spring")
+  }else if(str =="June" || str == "July" || str =="August"){
+      alert("Summer")
+  }else if (str =="September" || str == "October" || str =="November"){
+      alert("Autumn")
+  }else{
+      alert("Thats not a month")
+  }
 }
-console.log(capFrist("gud"));
-//24
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//25
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//26
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//27
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//28
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//29
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//30
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("nico"));
-//31
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//32
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//33
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//34
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//35
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("nico"));
-//36
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("nico"));
-//37
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("nico"));
-//38
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//39
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-//40
-function capFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(capFirst("gud"));
-*/
+seasons();
+Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+September, October or November, the season is Autumn.
+December, January or February, the season is Winter.
+March, April or May, the season is Spring
+June, July or August, the season is Summer
