d62265aafa63fa561935d17bd79a5b33f29608de
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-23 05:44:54 -0400

    the struggle

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-function numberOfDays() {
-  let query = prompt("What month is it?");
-  const year = parseInt(prompt("Enter a year:"));
-  let lowered = query.toLowerCase();
-  let month = lowered.charAt(0).toUpperCase() + lowered.slice(1);
-  if (
-    month == "January" ||
-    month == "March" ||
-    month == "May" ||
-    month == "July" ||
-    month == "August" ||
-    month == "October" ||
-    month == "December"
-  ) {
-    return `${month} has 31 days`;
-  }
-  if (
-    month == "April" ||
-    month == "June" ||
-    month == "September" ||
-    month == "November"
-  ) {
-    return `${month} has 30 days`;
-  }
-  if (
-    month === "February" &&
-    ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)
-  ) {
-    return `${month} has 29 days`;
-  }
-  if (month == "February") {
-    return `${month} has 28 days`;
-  }
+/* function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
-console.log(numberOfDays());
+console.log(capFirst("nico"));
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
+*/
+function capFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+console.log(capFirst("gud"));
