3a6bf73f90e5c7d766529295ffda528549750a44
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-22 21:03:43 -0400

    Js day 4

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-
+function isWeekend() {
+  let inquery = prompt("What day is today?");
+  let toLower = inquery.toLowerCase();
+  let day = toLower.charAt(0).toUpperCase() + toLower.slice(1);
+  if (
+    day == "Monday" ||
+    day == "Tuesday" ||
+    day == "Wednesday" ||
+    day == "Thursday" ||
+    day == "Friday"
+  ) {
+    alert(`What is the day today? ${inquery}
+    ${day} is a week day.`);
+  } else if (day == "Staturday" || day == "Sunday") {
+    alert(`What is the day today? ${inquery}
+    ${day} is a weekend.`);
+  } else {
+    alert(`${day} is not a real day try agian `);
+  }
+}
+isWeekend();
