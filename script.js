e8e8e6012b5170e4afad7394f36e6fd80121fcb6
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-23 05:23:55 -0400

    Js day 4

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-function isWeekend() {
-  let inquery = prompt("What day is today?");
-  let toLower = inquery.toLowerCase();
-  let day = toLower.charAt(0).toUpperCase() + toLower.slice(1);
+function numberOfDays() {
+  let query = prompt("What month is it?");
+  const year = parseInt(prompt("Enter a year:"));
+  let lowered = query.toLowerCase();
+  let month = lowered.charAt(0).toUpperCase() + lowered.slice(1);
   if (
-    day == "Monday" ||
-    day == "Tuesday" ||
-    day == "Wednesday" ||
-    day == "Thursday" ||
-    day == "Friday"
+    month == "January" ||
+    month == "March" ||
+    month == "May" ||
+    month == "July" ||
+    month == "August" ||
+    month == "October" ||
+    month == "December"
   ) {
-    alert(`What is the day today? ${inquery}
-    ${day} is a week day.`);
-  } else if (day == "Staturday" || day == "Sunday") {
-    alert(`What is the day today? ${inquery}
-    ${day} is a weekend.`);
-  } else {
-    alert(`${day} is not a real day try agian `);
+    return `${month} has 31 days`;
+  }
+  if (
+    month == "April" ||
+    month == "June" ||
+    month == "September" ||
+    month == "November"
+  ) {
+    return `${month} has 30 days`;
+  }
+  if (
+    month === "February" &&
+    ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)
+  ) {
+    return `${month} has 29 days`;
+  }
+  if (month == "February") {
+    return `${month} has 28 days`;
   }
 }
-isWeekend();
+console.log(numberOfDays());
