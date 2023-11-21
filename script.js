5faaf17195e7141e3df00a0b5a772f1ef9cdc11b
Nicolas Fuentes <nfuentes11@gmail.com>
2023-11-21 12:37:28 -0500

    Js Day 6

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // day 6 ex 2 num 1
+const characters =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+let charSplit = characters.split("");
+console.log(charSplit);
+let passCodeArr = [];
+let passCodeId;
+let random = Math.floor(Math.random() * 10 * 2);
+console.log(random);
+for (let i = 0; i < random; i++) {
+  let randomChar = Math.floor(Math.random() * charSplit.length);
+  passCodeArr.push(charSplit[randomChar]);
+}
+console.log(passCodeArr);
+passCodeId = passCodeArr.join("");
+console.log(passCodeId);
