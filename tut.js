e888dd543e732b1786211425cd4c8061bbac72d2
Nicolas Fuentes <nfuentes11@gmail.com>
2023-12-07 12:26:21 -0500

    Reduce()

--- tut.js
+++ tut.js
@@ -1,30 +1,16 @@
-const users = [
-  { firstName: "nico", lastName: "fuentes", age: 34 },
-  { firstName: "siggy", lastName: "acevedo", age: 36 },
-  { firstName: "didi", lastName: "fuentes", age: 3 },
-  { firstName: "milo", lastName: "fat", age: 10 },
+const items = [
+  { name: "Rice", price: 5 },
+  { name: "Book", price: 20 },
+  { name: "Chiken", price: 10 },
+  { name: "Monitor", price: 100 },
 ];
-let output = users.map((user) => user.firstName + " " + user.lastName);
-console.log(output);
-let output2 = users.reduce((acc, curr) => {
-  if (acc[curr.age]) {
-    acc[curr.age] = ++acc[curr.age];
-  } else {
-    acc[curr.age] = 1;
-  }
-  return acc;
-}, {});
-console.log(output2);
-let output3 = users
-  .filter((user) => user.age < 30)
-  .map((item) => item.firstName);
-console.log(output3);
-
-let output4 = users.reduce((acc, user) => {
-  if (user.age < 30) {
-    acc.push(user.firstName);
-  }
-  return acc;
-}, []);
-
-console.log(output4);
+// let totalPrice = 0;
+// items.forEach((item) => {
+//   totalPrice += item.price;
+// });
+let totalPrice = items.reduce((total, item) => {
+  console.log(`Total:${total}`);
+  console.log(`item: ${item.price}`);
+  return total + item.price;
+}, 0);
+console.log(totalPrice);
