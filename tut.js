e7d827fa965ed15626067219f6159ad157a19028
Nicolas Fuentes <nfuentes11@gmail.com>
2023-12-08 13:05:03 -0500

    Arr methods

--- tut.js
+++ tut.js
@@ -1,5 +1,29 @@
-const numbers = [3, 6, 2, 9, 1];
-const sum = numbers.reduce((total, number) => {
-  return total + number;
-}, 0);
-console.log(sum);
+const people = [
+  {
+    name: "Nico Fuentes",
+    age: 34,
+    occupation: "Web Developer",
+  },
+  {
+    name: "Didi TinTin",
+    age: 3,
+    occupation: "Everything All at once",
+  },
+  {
+    name: "Siggy Acevedo",
+    age: 36,
+    occupation: "Records",
+  },
+  {
+    name: "Milo Fuentes",
+    age: 10,
+    occupation: "Fat",
+  },
+];
+
+const nameArr = people.map((person) => person.name);
+console.log(nameArr);
+const onlyThirty = people.filter((person) => {
+  return person.age > 30;
+});
+console.log(onlyThirty);
