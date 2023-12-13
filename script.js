cf214a67d7df7e9d7282986cbc093874ac9257fd
Nicolas Fuentes <nfuentes11@gmail.com>
2023-12-13 10:03:29 -0500

    js day 7

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //js day 07
+function fullName(first, last) {
+  return ` ${first} ${last}`;
+}
+console.log(fullName("Didi", "TinTin"));
+const sum = (a, b) => a + b;
+console.log(sum(1, 10));
+const areaOfRectangle = (l, w) => l * w;
+const perimeterOfRectangle = (l, w) => 2 * (l + w);
+console.log(perimeterOfRectangle(2, 5));
+const volumeOfRectPrism = (l, w, h) => l * w * h;
+const areaOfCircle = (r) => Math.PI * r * r;
+const circumOfCircle = (r) => 2 * (Math.PI * r);
+const density = (m, v) => m / v;
+const speed = (d, t) => d / t;
+const weight = (m, g) => m * g;
+const convertCelsiusToFahrenheit = (c) => c * (9 / 5) + 32;
+console.log(convertCelsiusToFahrenheit(40));
+const BMI = (w, h) => {
+  let cal = w / (w * h);
+  if (cal < 18.5) {
+    return `Your BMI is ${cal} you are Underweight `;
+  } else if (cal <= 24.9) {
+    return `Your BMI is ${cal} you are Normal `;
+  } else if (cla < 29.9) {
+    return `Your BMI is ${cal} you are Overweight `;
+  } else {
+    return `Your BMI is ${cal} you are Obese `;
+  }
+};
+console.log(BMI(90, 1.69));
