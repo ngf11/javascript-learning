13c018b607904b7cc2c2d858caf38f1c11a0998d
Nicolas Fuentes <nfuentes11@gmail.com>
2023-12-20 13:04:31 -0500

    async await

--- tut.js
+++ tut.js
@@ -1,12 +1,18 @@
-function getData() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Hello mate!!");
-    }, 100);
-  });
-}
 async function start() {
-  const result = await getData();
-  console.log(result);
+  const data = await fetch(
+    "https://api.soccerdataapi.com/livescores/?auth_token=7336b77d2f4f9c28740cb42868508ed17e8ce4df"
+  );
+  const result = await data.json();
+  const output = result.map((item) => item.country);
+  console.log(output);
+}
+
+function start2() {
+  return fetch(
+    "https://api.soccerdataapi.com/livescores/?auth_token=7336b77d2f4f9c28740cb42868508ed17e8ce4df"
+  )
+    .then((response) => response.json())
+    .then((data) => data.map((items) => console.log(items.league_name)));
 }
 start();
+start2();
