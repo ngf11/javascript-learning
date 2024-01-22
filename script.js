fb0ca3b3c9e0522f0fe5b954282967a25d5d64b0
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-22 12:50:05 -0500

    Js Day 08

--- script.js
+++ script.js
@@ -1,62 +1,23 @@
-function randomHexColorGenerator() {
-  let hex = "#";
-  for (let i = 0; i < 6; i++) {
-    hex += Math.floor(Math.random() * 16).toString(16);
-  }
-  return hex;
-}
-console.log(randomHexColorGenerator());
-const userIdGenerator = () => {
-  let ID = "";
-  let char = "abcdefghijklmnopqrstuvxxwzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
-  let charArr = char.split("");
-  for (let i = 0; i <= 7; i++) {
-    let random = Math.floor(Math.random() * charArr.length);
-
-    ID += charArr[random];
-  }
-  return ID;
+//day 08
+const person = {
+  firstName: "Asabeneh",
+  lastName: "Yetayeh",
+  age: 250,
+  country: "Finland",
+  city: "Helsinki",
+  skills: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Node",
+    "MongoDB",
+    "Python",
+    "D3.js",
+  ],
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  "phone number": "+3584545454545",
 };
-console.log(userIdGenerator());
-const userIdGeneratedByUser = () => {
-  // let idLength = prompt("How long you want your id's?");
-  // let numberOfId = prompt("How many new id's?");
-  let idLength = 5;
-  let numberOfId = 5;
-  let char = "abcdefghijklmnopqrstuvxxwzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
-  let charArr = char.split("");
-  let idArr = [];
-  for (let i = 0; i <= numberOfId; i++) {
-    let ID = "";
-    for (let j = 0; j <= idLength; j++) {
-      let random = Math.floor(Math.random() * charArr.length);
-
-      ID += charArr[random];
-    }
-    idArr.push(ID);
-  }
-  return idArr;
-};
-console.log(userIdGeneratedByUser());
-const rgbColorGenerator = () => {
-  let rgbArr = [];
-  for (let i = 0; i < 3; i++) {
-    let random = Math.floor(Math.random() * 256);
-    rgbArr.push(random);
-  }
-  let rgb = `rgb(${rgbArr})`;
-  return rgb;
-};
-console.log(rgbColorGenerator());
-const arrayOfHexaColors = (num) => {
-  let hexArr = [];
-  for (let i = 0; i < num; i++) {
-    let hex = "#";
-    for (let j = 0; j < 6; j++) {
-      hex += Math.floor(Math.random() * 16).toString(16);
-    }
-    hexArr.push(hex);
-  }
-  return hexArr;
-};
-console.log(arrayOfHexaColors(5));
+console.log(person.getFullName());
---
+++ tut.js
@@ -0,0 +1,6 @@
+// short circut
+console.log(true || "hello");
+console.log(false || "hello");
+console.log("--------");
+console.log(true && "hello");
+console.log(false && "hello");
