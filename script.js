fe791a475dd2e9385e0bb489acad799722a8ff75
Nicolas Fuentes <nfuentes11@gmail.com>
2024-02-16 20:13:07 -0500

    js DAY 08

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ dog.name = "Milo";
 dog.color = "White";
 dog.age = 11;
 dog.bark = "Woof";
-dog.greet = () => console.log(this.bark);
+dog.greet = function () {
+  console.log(this.bark);
+};
 console.log(dog);
 dog.greet();
+dog.breed = "Bulldog";
+dog.getDoginfo = function () {
+  console.log(` Hi ${this.name} i am a ${this.age} years old ${this.breed}`);
+};
+dog.getDoginfo();
