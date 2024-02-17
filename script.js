2539974f731beb677a80068295e8647e49a50ed3
Nicolas Fuentes <nfuentes11@gmail.com>
2024-02-16 20:05:37 -0500

    js DAY 08

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const dog = {};
+console.log(dog);
+dog.name = "Milo";
+dog.color = "White";
+dog.age = 11;
+dog.bark = "Woof";
+dog.greet = () => console.log(this.bark);
+console.log(dog);
+dog.greet();
