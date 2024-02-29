370b85c52cf2d7e6b04567762b4a3e13b6f92989
Nicolas Fuentes <nfuentes11@gmail.com>
2024-02-28 22:30:02 -0500

    Once more

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ console.log(year);
 console.log(day);
 console.log(month);
 console.log(`${hh}:${mm}`);
+const areaOfTriangle = (b, h) => {
+  return `Enter base: ${b}\nEnter height: ${h}\nThe area of the triangle is ${
+    b * h * 0.5
+  }`;
+};
+console.log(areaOfTriangle(10, 5));
