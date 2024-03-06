f7a3ea1d2789964994882da552268bbc59a823af
Nicolas Fuentes <nfuentes11@gmail.com>
2024-03-05 20:36:09 -0500

    At it agian

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let grade = prompt("What your grade", " in numbers");
+let grade = prompt("What is your grade", " in numbers");
 switch (true) {
   case grade > 90 && grade < 100:
     console.log("A");
@@ -6,13 +6,15 @@ switch (true) {
   case grade > 80:
     console.log("B");
     break;
-  case grade > 90:
-    console.log("A");
+  case grade > 70:
+    console.log("C");
     break;
-  case grade > 90:
-    console.log("A");
+  case grade > 60:
+    console.log("D");
     break;
-  case grade > 90:
-    console.log("A");
+  case grade < 50:
+    console.log("F");
     break;
+  default:
+    console.log("Thats not a valid grade");
 }
