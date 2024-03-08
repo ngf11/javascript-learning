ce199ec72df8cf030848a5d96dbc13ff23e0e944
Nicolas Fuentes <nfuentes11@gmail.com>
2024-03-07 23:25:14 -0500

    Js day 4

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
-// let grade = prompt("What is your grade", " in numbers");
-// switch (true) {
-//   case grade > 90 && grade < 100:
-//     console.log("A");
-//     break;
-//   case grade > 80:
-//     console.log("B");
-//     break;
-//   case grade > 70:
-//     console.log("C");
-//     break;
-//   case grade > 60:
-//     console.log("D");
-//     break;
-//   case grade < 50:
-//     console.log("F");
-//     break;
-//   default:
-//     console.log("Thats not a valid grade");
-// }
-const seasons = () => {
-  let promptMonth = prompt("What month is it?");
-  let lowerCase = promptMonth.toLowerCase();
-  let firstChar = (lowerCase) =>
-    lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
-  switch (firstChar(lowerCase)) {
-    case "September" || "October" || "November":
-      console.log("Autumn");
-      break;
-    case "December" || "January" || "February":
-      console.log("Winter");
-      break;
-    case "March" || "April" || "May":
-      console.log("Spring");
-    case "June" || "July" || "August":
-      console.log("Summer");
-      break;
-    default:
-      console.log("Not a valid month");
-  }
+const whatDayIsIt = () => {
+  let day = prompt("What day is it?");
+  let dayLowered = day.toLowerCase();
+  let dayCap = (dayLowered) => {
+    return dayLowered.charAt(0).toUpperCase() + dayLowered.slice(1);
+  };
+  return dayCap(dayLowered) == "Monday" ||
+    "Tusday" ||
+    "Wednesday" ||
+    "Tursday" ||
+    "Friday"
+    ? console.log("It's a Weekday")
+    : console.log("its the weekend");
 };
-
-seasons();
+whatDayIsIt();
