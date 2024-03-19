0de6ec8b7fdd7f4cf99a26a506544ffbcd24b568
Nicolas Fuentes <nfuentes11@gmail.com>
2024-03-18 21:40:22 -0400

    Js day 5

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
-const whatDayIsIt = () => {
-  let day = prompt("What day is it?");
-  let dayLowered = day.toLowerCase();
-  let dayCap = (dayLowered) => {
-    return dayLowered.charAt(0).toUpperCase() + dayLowered.slice(1);
-  };
-  return dayCap(dayLowered) == "Monday" ||
-    "Tusday" ||
-    "Wednesday" ||
-    "Tursday" ||
-    "Friday"
-    ? console.log("It's a Weekday")
-    : console.log("its the weekend");
-};
-whatDayIsIt();
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+const emptyArr = [];
+console.log(emptyArr);
+const numberArr = [1, 2, 3, 4, 5];
+// console.log(numberArr.length);
+console.log(numberArr[0]);
+console.log(numberArr[(numberArr.length - 1) / 2]);
+console.log(numberArr[numberArr.length - 1]);
+const mixedDataTypes = ["nico", "fuentes", true, null, 35];
+const itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
+console.log(mixedDataTypes);
+console.log(itCompanies);
+console.log(itCompanies.length);
+console.log(itCompanies[0]);
+console.log(itCompanies[(itCompanies.length - 1) / 2]);
+console.log(itCompanies[itCompanies.length - 1]);
+// console.log("itCompanies".toUpperCase());
+for (company of itCompanies) {
+  console.log(company.toUpperCase());
+}
+console.log(
+  itCompanies
+    .splice(itCompanies.length - 1, 1, " and Amazon are big IT companies.")
+    .join(" ")
+);
